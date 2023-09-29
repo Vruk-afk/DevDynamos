c34f4be7767f9d7ac8ac17cbf554fede05335b20
Vruk-afk <utkarshzerotwo@gmail.com>
2023-09-29 13:04:30 +0530

    Final

--- src/src/urls.py
+++ src/src/urls.py
@@ -1,41 +1,29 @@
-"""edu_fcih URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import handler404, handler500, handler400
+from django.conf import settings
 from django.conf.urls.static import static
-from .settings import *
-from django.conf.urls import include
-from . import settings
-from accounts.views import index
+
+
 urlpatterns = [
-    path('admininstrator/', admin.site.urls),
+    path('', include('app.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('programs/', include('course.urls')),
+    path('result/', include('result.urls')),
+    path('search/', include('search.urls')),
+    path('quiz/', include('quiz.urls')),
+
+    path('payments/', include('payments.urls')),
+
+    path('accounts/api/', include('accounts.api.urls', namespace='accounts-api')),
+
+    path('admin/', admin.site.urls),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-        path('admin/',include('admin_dashboard.urls',namespace='admin-dashboard')),
-        path('auth/',include('accounts.urls',namespace='accounts')),
-        path('admin/message/',include('chat_system.urls',namespace='chat_system')),
-        path('admin/announcement/',include('announcement.urls',namespace='announcement')),
-        path('admin/course/',include('courses.urls',namespace='courses')),
-        path('student/',include('student_dashboard.urls',namespace='student-dashboard')),
-        path('doctor/',include('doctor_dashboard.urls',namespace='doctor-dashboard')),
-        path('',index),
-
-
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# handler404 = 'app.views.handler404'
+# handler500 = 'app.views.handler500'
+# handler400 = 'app.views.handler400'
